61ed0e1e62dff68ff596fd98e51fce3d30512262
EmirioBomb <wjhlfamily@gmail.com>
2026-05-09 09:25:25 +0800

    chore(tsconfig): :wrench:  add css module declarations in d.ts

--- docs/.vuepress/theme/shim.d.ts
+++ docs/.vuepress/theme/shim.d.ts
@@ -4,3 +4,8 @@ declare module '*.vue' {
   const comp: ComponentOptions
   export default comp
 }
+
+declare module '*.css'
+declare module '*.scss'
+declare module '*.sass'
+declare module '*.less'
